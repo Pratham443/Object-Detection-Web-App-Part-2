594d54fc502125e18f2e7306554fcbf3b0d3dcfe
Pratham443 <77573159+Pratham443@users.noreply.github.com>
2021-08-04 17:52:02 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,32 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("desk.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 456);
+    canvas.position(450, 250);
+
+    cocossd = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 456)
+}
+
+function modelLoaded() {
+    console.log("COCO SSD initialized");
+    status = true;
+    cocossd.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error)
+    }
+
+    console.log(results);
+}
